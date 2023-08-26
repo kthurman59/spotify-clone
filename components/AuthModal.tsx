0da0c2f2c81dc00e2e1611db4019382a371aac5e
Errantghost <25540190+kthurman59@users.noreply.github.com>
2023-08-26 04:11:59 -0500

    fine tuning Auth/tracking an issue

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -2,11 +2,34 @@
 
 import { Children } from "react";
 import Modal from "./Modal";
+import { useSupabaseClient, useSessionContext } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 const AuthModal = () => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+
   return (
     <Modal title="Welcome Back!" description="Login to your account" isOpen onChange={() => {}}>
-      Auth modal children!
+      <Auth theme="dark"
+        magicLink
+        providers={["github"]} 
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default:{
+              colors: {
+                brand: '#404040',
+                brandAccent: '#22c55e'
+              }
+            }
+          }
+      }}
+    />
     </Modal>
   );
 }
