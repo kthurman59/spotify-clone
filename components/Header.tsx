90f53259c30c576fc0691558e54b5618fd84373f
Errantghost <25540190+kthurman59@users.noreply.github.com>
2023-08-28 12:57:49 -0500

    search by title

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,144 +1,165 @@
 "use client";
 
 import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
-import { BiSearch } from "react-icons/bi";
-import { HiHome } from "react-icons/hi";
-import { RxCaretLeft } from "react-icons/rx"
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useUser } from "@/hooks/useUser";
 import { FaUserAlt } from "react-icons/fa";
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import { toast } from "react-hot-toast";
-
-import Button from "./Button";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
 import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
 }
 
-
 const Header: React.FC<HeaderProps> = ({
-    children,
-    className
+  children,
+  className,
 }) => {
-  const authModal = useAuthModal();
   const router = useRouter();
+  const authModal = useAuthModal();
 
   const supabaseClient = useSupabaseClient();
   const { user } = useUser();
 
-  const hanldeLogout = async () => {
-    const {error} = await supabaseClient.auth.signOut();
-    // TODO: reset any playing songs future
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
     router.refresh();
 
     if (error) {
       toast.error(error.message);
-    } else {
-      toast.success('Logged out!')
     }
   }
 
   return (
-    <div className={twMerge(`
-      h-fit
-      bg-gradient-to-b
-      from-emerald-800
-      p-6
-    `,
-      className
-      )}
-    >
-        <div className="
-          w-full
-          mb-4
-          flex
-          items-center
-          justify-between
-        ">
+    <div
+      className={twMerge(`
+        h-fit 
+        bg-gradient-to-b 
+        from-emerald-800 
+        p-6
+        `,
+        className
+      )}>
+      <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button>
-            <RxCaretLeft size={35}  />
+          <button 
+            onClick={() => router.back()} 
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button 
+            onClick={() => router.forward()} 
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretRight className="text-white" size={35} />
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-          <button
+          <button 
+            onClick={() => router.push('/')} 
             className="
-              rounded-full
-              p-2
-              bg-white
-              flex
-              items-center
-              justify-center
-              hover:opacity-75
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
               transition
             "
           >
             <HiHome className="text-black" size={20} />
           </button>
-          <button
+          <button 
+            onClick={() => router.push('/search')} 
             className="
-              rounded-full
-              p-2
-              bg-white
-              flex
-              items-center
-              justify-center
-              hover:opacity-75
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
               transition
             "
           >
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
-        <div
-          className="
-            flex
-            justify-between
-            items-center
-            gap-x-4
-          "
-          >
-            {user ? (
-              <div className="flex gap-x-4 items-center">
-                <Button onClick={hanldeLogout}
-                  className="bg-white px-6 py-2"
-                >
-                  Logout
-                </Button>
+        <div className="flex justify-between items-center gap-x-4">
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button 
+                onClick={handleLogout} 
+                className="bg-white px-6 py-2"
+              >
+                Logout
+              </Button>
+              <Button 
+                onClick={() => router.push('/account')} 
+                className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          ) : (
+            <>
+              <div>
                 <Button 
-                  onClick={() => router.push('/account')}
-                  className="bg-white"
+                  onClick={authModal.onOpen} 
+                  className="
+                    bg-transparent 
+                    text-neutral-300 
+                    font-medium
+                  "
                 >
-                  <FaUserAlt />
+                  Sign up
                 </Button>
               </div>
-            ) : (
-           <>
-           <div>
-             <Button
-             onClick={authModal.onOpen}
-             className="bg-transparent text-neutral-300 font-medium">
-              Sign Up
-             </Button>
-           </div>
-           <div>
-             <Button
-               onClick={authModal.onOpen}
-               className="bg-white px-6 py-2">
-              Log in
-             </Button>
-           </div>
-           </>
-           )}
+              <div>
+                <Button 
+                  onClick={authModal.onOpen} 
+                  className="bg-white px-6 py-2"
+                >
+                  Log in
+                </Button>
+              </div>
+            </>
+          )}
         </div>
-        </div>
-        {children}
+      </div>
+      {children}
     </div>
-  )
+  );
 }
 
 export default Header;
